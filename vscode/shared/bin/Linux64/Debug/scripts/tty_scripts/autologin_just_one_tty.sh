12bfbdab93476341b5604484a95909919936bf38
reisdout <reisdout@gmail.com>
2025-02-07 11:20:26 -0300

    parando a tty antes de configurar no enable une tty sh

--- vscode/shared/bin/Linux64/Debug/scripts/tty_scripts/autologin_just_one_tty.sh
+++ vscode/shared/bin/Linux64/Debug/scripts/tty_scripts/autologin_just_one_tty.sh
@@ -7,12 +7,15 @@ echo ${n_tty}
 # ExecStart=
 # ExecStart=-/sbin/mingetty --autologin ns --noclear ${n_tty}
 # _EOF_
+
+com_stop_tty="$(printf '%s%s%s\n' 'sudo systemctl stop getty@tty'$1'.service')"
+${com_stop_tty}
+
 sudo mkdir -p /etc/systemd/system/getty@tty$1.service.d
 echo "[Service]" > /etc/systemd/system/getty@tty$1.service.d/override.conf
 echo "ExecStart=" >> /etc/systemd/system/getty@tty$1.service.d/override.conf
 echo "ExecStart=-/sbin/mingetty --autologin ns --noclear tty$1" >> /etc/systemd/system/getty@tty$1.service.d/override.conf
 echo "_EOF_" >> /etc/systemd/system/getty@tty$1.service.d/override.conf
-com_stop_tty="$(printf '%s%s%s\n' 'sudo systemctl stop getty@tty'$1'.service')"
+
 com_start_tty="$(printf '%s%s%s\n' 'sudo systemctl start getty@tty'$1'.service')"
-${com_stop_tty} 
 ${com_start_tty}
