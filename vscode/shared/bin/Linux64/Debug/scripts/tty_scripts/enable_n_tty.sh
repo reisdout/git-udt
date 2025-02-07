227621dad42b818d6b5ca7601c938bc9cde719cc
reisdout <reisdout@gmail.com>
2025-02-07 14:32:28 -0300

    ajustando habilitacao das tty menores que 7

--- vscode/shared/bin/Linux64/Debug/scripts/tty_scripts/enable_n_tty.sh
+++ vscode/shared/bin/Linux64/Debug/scripts/tty_scripts/enable_n_tty.sh
@@ -1,9 +1,17 @@
 echo "Enter the value of n:"
 read n
 n=`expr $n + 1` # para o looping ir até n
-i=8
+i=2
+graph_int=7
 while [ $i -lt $n ]
 do
+
+   if [ $i -eq $graph_int ];  #pulando a interface gráfica
+        then
+            echo "Skiping GUI" 
+            i=`expr $i + 1`    
+            continue
+   fi
   #echo "tty${i}";
   echo  "Criando a tty${i}..."
   comn="sudo systemctl start getty@tty${i}.service";
@@ -12,7 +20,7 @@ do
   #echottycc=`$echotty`;
   b=`$comn`;
   echo ${b};
-  sleep 1;
+  #sleep 1;
   $( (printf ${echotty}) >  /dev/tty${i})
   #$(ls > "/dev/tty${i}")
   #echo ${b};
